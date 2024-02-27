745c39ec174bc06584edfb6aa2d5efcd21fad51d
HugoRCD <hugo.richard@epitech.eu>
2024-02-27 16:03:33 +0100

    Add favicon, and app title

--- app.vue
+++ app.vue
@@ -1,4 +1,23 @@
 <script setup lang="ts">
+const { title, link } = useAppConfig();
+useHead({
+  title: title,
+  link: link,
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" },
+    { name: "author", content: "Hugo Richard" },
+    { charset: "utf-8" },
+    {
+      name: "description",
+      content:
+        "Currencia is the simplest crypto tracker, with a clean and minimalistic design, you can quickly be up to date with the latest crypto news and prices.",
+    },
+    {
+      name: "keywords",
+      content: "crypto, tracker, bitcoin, ethereum, litecoin, dogecoin, ripple, binance, coinbase, kraken, crypto news, crypto prices",
+    },
+  ],
+});
 </script>
 
 <template>
