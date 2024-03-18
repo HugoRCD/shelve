0d627eaad5de9fda1c161dea9509710eb35d7c40
HugoRCD <hugo.richard@epitech.eu>
2024-03-18 16:38:47 +0100

    Some fixes

--- apps/app/server/app/variableService.ts
+++ apps/app/server/app/variableService.ts
@@ -2,7 +2,6 @@ import { Environment, type VariablesCreateInput } from "@shelve/types";
 import prisma from "~/server/database/client";
 import { decrypt, encrypt } from "@shelve/utils";
 
-
 export async function upsertVariable(variablesCreateInput: VariablesCreateInput) {
   const runtimeConfig = useRuntimeConfig().private;
   if (variablesCreateInput.variables.length === 1) {
@@ -10,10 +9,6 @@ export async function upsertVariable(variablesCreateInput: VariablesCreateInput)
       const encryptedValue = encrypt(variable.value, runtimeConfig.secret_encryption_key, parseInt(runtimeConfig.secret_encryption_iterations));
       const { index, ...rest} = variable;
       rest.value = encryptedValue;
-      return rest;
-    });
-    variablesCreateInput.variables = variablesCreateInput.variables.map((variable) => {
-      const { index, ...rest } = variable;
       return rest;
     });
     const variableCreateInput = variablesCreateInput.variables[0];
@@ -26,15 +21,11 @@ export async function upsertVariable(variablesCreateInput: VariablesCreateInput)
     });
   } else {
     variablesCreateInput.variables = variablesCreateInput.variables.map((variable) => {
-      const { index, ...rest } = variable;
-      return rest;
-    });
-    /*variablesCreateInput.variables = variablesCreateInput.variables.map((variable) => {
       const encryptedValue = encrypt(variable.value, runtimeConfig.secret_encryption_key, parseInt(runtimeConfig.secret_encryption_iterations));
       const { index, ...rest } = variable;
       rest.value = encryptedValue;
       return rest;
-    });*/
+    });
     return prisma.envVar.createMany({
       data: variablesCreateInput.variables,
       skipDuplicates: true,
@@ -43,21 +34,21 @@ export async function upsertVariable(variablesCreateInput: VariablesCreateInput)
 }
 
 export async function getVariablesByProjectId(projectId: number, environment?: Environment) {
+  const runtimeConfig = useRuntimeConfig().private;
   const options = environment ? {
     projectId,
     environment: {
       contains: environment,
     }
   } : { projectId };
-  /*const variables = await prisma.envVar.findMany({ where: options, orderBy: { updatedAt: "desc" } });
+  const variables = await prisma.envVar.findMany({ where: options, orderBy: { updatedAt: "desc" } });
   return variables.map((variable) => {
     const decryptedValue = decrypt(variable.value, runtimeConfig.secret_encryption_key, parseInt(runtimeConfig.secret_encryption_iterations));
     return {
       ...variable,
       value: decryptedValue,
     };
-  });*/
-  return prisma.envVar.findMany({ where: options, orderBy: { updatedAt: "desc" } });
+  });
 }
 
 export async function deleteVariable(id: number, environment: string) {
