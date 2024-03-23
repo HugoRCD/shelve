a4a4c5493cd7e7803832b4d71df66c723b0f81a9
HugoRCD <hugo.richard@epitech.eu>
2024-03-23 14:07:10 +0100

    Remove log

--- apps/app/server/api/auth/login.post.ts
+++ apps/app/server/api/auth/login.post.ts
@@ -5,7 +5,6 @@ import { H3Event } from "h3";
 export default eventHandler(async (event: H3Event) => {
   const body = await readBody(event);
   body.authToken = getCookie(event, "authToken") || "";
-  console.log(body);
   const { user, authToken} = await login(body);
   setCookie(event, "authToken", authToken, {
     httpOnly: true,
