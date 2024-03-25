278fe7c228088ecbd6c57f73b9e48413a93d6ea2
HugoRCD <hugo.richard@epitech.eu>
2024-03-25 15:48:09 +0100

    temporary otp email fix

--- apps/app/server/app/resendService.ts
+++ apps/app/server/app/resendService.ts
@@ -6,12 +6,17 @@ const resend = new Resend(process.env.NUXT_PRIVATE_RESEND_API_KEY);
 export async function sendOtp(email: string, otp: string) {
   const runtimeConfig = useRuntimeConfig();
   const siteUrl = runtimeConfig.public.siteUrl;
-  const template = await useCompiler('verify-otp.vue', {
-    props: {
-      otp,
-      redirectUrl: `${siteUrl}/login?email=${email}&otp=${otp}`,
-    }
-  });
+  let template = { html: '' };
+  try {
+    template = await useCompiler('verify-otp.vue', {
+      props: {
+        otp,
+        redirectUrl: `${siteUrl}/login?email=${email}&otp=${otp}`,
+      }
+    });
+  } catch (error) {
+    template.html = `<h1>OTP: ${otp}</h1>`;
+  }
 
   try {
     await resend.emails.send({
