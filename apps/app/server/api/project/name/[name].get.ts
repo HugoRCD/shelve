7e71cb09f193b45db07bfa84a99c6cc4797467ca
HugoRCD <hugo.richard@epitech.eu>
2024-03-31 13:03:54 +0200

    fix: link command on composed project name #58

--- apps/app/server/api/project/name/[name].get.ts
+++ apps/app/server/api/project/name/[name].get.ts
@@ -2,12 +2,12 @@ import { H3Event } from 'h3'
 import prisma from '~/server/database/client'
 
 export default eventHandler(async (event: H3Event) => {
-  const name = getRouterParam(event, 'name') as string
-  if (!name) throw createError({ statusCode: 400, statusMessage: 'Missing params' })
+  const paramName = getRouterParam(event, 'name')
+  if (!paramName) throw createError({ statusCode: 400, statusMessage: 'Missing params' })
   return await prisma.project.findFirst({
     where: {
       name: {
-        equals: name,
+        equals: decodeURIComponent(paramName),
         mode: 'insensitive',
       },
     },
