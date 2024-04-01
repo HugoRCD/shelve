d9b75a43c278c247a59f72bbd8087240eb43b018
HugoRCD <hugo.richard@epitech.eu>
2024-04-01 12:21:24 +0200

    feat: add open and upgrade command

---
+++ apps/cli/src/commands/upgrade.ts
@@ -0,0 +1,28 @@
+import { defineCommand } from 'citty'
+import consola from 'consola'
+import { execa } from 'execa'
+import { name, version } from '../../package.json'
+import { isLatestVersion } from '../utils/update'
+
+export default defineCommand({
+  meta: {
+    name: 'upgrade',
+    description: 'Upgrade the CLI to the latest version',
+  },
+  async setup() {
+    const latestVersion = await isLatestVersion()
+    if (!latestVersion) {
+      consola.success('You are using the latest version of Shelve CLI')
+    } else {
+      consola.start(`Upgrading from ${version} to the latest version...`)
+      try {
+        await execa('npm', ['install', '-g', name], { stdio: 'inherit' })
+        consola.success(`Successfully upgraded to the latest version`)
+      } catch (error) {
+        consola.error('Failed to upgrade the CLI')
+        consola.error(error)
+
+      }
+    }
+  },
+})
