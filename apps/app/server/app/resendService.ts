93ddeb5d9e1b54b8e95606a0cd484a6cb47bfc3c
HugoRCD <hugo.richard@epitech.eu>
2024-06-26 14:47:10 +0200

    test fix

--- apps/app/server/app/resendService.ts
+++ apps/app/server/app/resendService.ts
@@ -2,11 +2,10 @@ import { Resend } from 'resend'
 import { render } from '@vue-email/render'
 import verifyOtp from '../emails/verifyOtp.vue'
 
-const runtimeConfig = useRuntimeConfig()
-
-const resend = new Resend(runtimeConfig.private.resendApiKey)
 
 export async function sendOtp(email: string, otp: string) {
+  const runtimeConfig = useRuntimeConfig()
+  const resend = new Resend(runtimeConfig.private.resendApiKey)
   const { siteUrl } = runtimeConfig.public
   let template
   try {
