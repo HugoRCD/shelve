6b4335f467faed6ca6ae4c76af2b4fef8d410a4f
HugoRCD <hugo.richard@epitech.eu>
2024-07-08 15:38:57 +0200

    Use cache for team relate feature

--- apps/app/server/api/teams/index.get.ts
+++ apps/app/server/api/teams/index.get.ts
@@ -1,29 +1,7 @@
 import { H3Event } from 'h3'
-import prisma from '~~/server/database/client'
+import { getTeamByUserId } from '~~/server/app/teamsService'
 
-export default eventHandler(async (event: H3Event) => {
+export default eventHandler((event: H3Event) => {
   const { user } = event.context
-  return await prisma.team.findMany({
-    where: {
-      members: {
-        some: {
-          userId: user.id,
-        },
-      }
-    },
-    include: {
-      members: {
-        include: {
-          user: {
-            select: {
-              id: true,
-              username: true,
-              email: true,
-              avatar: true,
-            }
-          }
-        }
-      }
-    }
-  })
+  return getTeamByUserId(user.id)
 })
