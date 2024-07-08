edfb6ff42d7f605ea09226232a9ab06f10256486
Hugo Richard <hugo.richard@epitech.eu>
2024-07-08 15:39:47 +0200

    feat: Use cache for team relate feature (#157)

--- apps/app/server/app/teamsService.ts
+++ apps/app/server/app/teamsService.ts
@@ -1,7 +1,8 @@
 import { type CreateTeamInput, TeamRole } from '@shelve/types'
 import prisma from '~~/server/database/client'
 
-export function createTeam(createTeamInput: CreateTeamInput, userId: number) {
+export async function createTeam(createTeamInput: CreateTeamInput, userId: number) {
+  await deleteCachedTeamByUserId(userId)
   return prisma.team.create({
     data: {
       name: createTeamInput.name,
@@ -60,6 +61,7 @@ export async function upsertMember(teamId: number, addMemberInput: {
     },
   })
   if (!user) throw new Error('user not found')
+  await deleteCachedTeamByUserId(user.id)
   return prisma.member.upsert({
     where: {
       id: team.members.find((member) => member.userId === user.id)?.id || -1,
@@ -100,6 +102,7 @@ export async function removeMember(teamId: number, memberId: number, requesterId
     },
   })
   if (!team) throw new Error('unauthorized')
+  await deleteCachedTeamByUserId(requesterId)
   return prisma.member.delete({
     where: {
       id: memberId,
@@ -120,9 +123,44 @@ export async function deleteTeam(teamId: number, userId: number) {
     },
   })
   if (!team) throw new Error('unauthorized')
+  await deleteCachedTeamByUserId(userId)
   return prisma.team.delete({
     where: {
       id: teamId,
     },
   })
+}
+
+export const getTeamByUserId = cachedFunction((userId: number) => {
+  return prisma.team.findMany({
+    where: {
+      members: {
+        some: {
+          userId: userId,
+        },
+      }
+    },
+    include: {
+      members: {
+        include: {
+          user: {
+            select: {
+              id: true,
+              username: true,
+              email: true,
+              avatar: true,
+            }
+          }
+        }
+      }
+    }
+  })
+}, {
+  maxAge: 60 * 60,
+  name: 'getTeamByUserId',
+  getKey: (userId: number) => `userId:${userId}`,
+})
+
+export function deleteCachedTeamByUserId(userId: number) {
+  return useStorage('cache').removeItem(`nitro:functions:getTeamByUserId:userId:${userId}.json`)
 }
