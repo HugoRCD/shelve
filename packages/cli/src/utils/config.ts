e57de89bd277a96664cc4b666befb7194616e47b
HugoRCD <hugo.richard@epitech.eu>
2024-09-10 15:58:00 +0200

    wip

--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -1,13 +1,31 @@
-import { loadConfig, watchConfig } from 'c12'
+import { loadConfig } from 'c12'
 
 export async function showConfig() {
-  const { config } = await loadConfig({
+  const { config, configFile } = await loadConfig({
     name: 'shelve',
     packageJson: true,
     defaultConfig: {
       token: process.env.SHELVE_TOKEN,
       url: process.env.SHELVE_URL || 'https://shelve.hrcd.fr',
     },
+    dotenv: true,
   })
   console.log(config)
+  console.log(configFile)
+}
+
+type ShelveConfig = {
+  token: string
+  url: string
+}
+
+export async function defineShelveConfig(config: ShelveConfig) {
+  const { configFile } = await loadConfig({
+    name: 'shelve',
+    packageJson: true,
+    defaultConfig: config,
+    dotenv: true,
+  })
+
+  return configFile
 }
