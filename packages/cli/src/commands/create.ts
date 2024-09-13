16dad7911ff18f755d50ddd4a2ecbad757d52212
HugoRCD <hugo.richard@epitech.eu>
2024-09-13 13:55:14 +0200

    Add -n option

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -10,18 +10,22 @@ export function createCommand(program: Command): void {
     .alias('c')
     .alias('init')
     .description('Create a new project')
-    .action(async () => {
-      intro('Create a new project')
+    .option('-n, --name <name>', 'Name of the project')
+    .action(async (options) => {
+      let { name } = options
+      intro(name ? `Creating project ${name}` : 'Creating a new project')
 
-      const name = await text({
-        message: 'Enter the name of the project:',
-        placeholder: 'my-project',
-        validate(value) {
-          if (value.length === 0) return `Value is required!`
-        },
-      })
+      if (!name) {
+        name = await text({
+          message: 'Enter the name of the project:',
+          placeholder: 'my-project',
+          validate(value) {
+            if (value.length === 0) return `Value is required!`
+          },
+        })
 
-      if (isCancel(name)) onCancel('Operation cancelled.')
+        if (isCancel(name)) onCancel('Operation cancelled.')
+      }
 
       await createProject(name)
 
