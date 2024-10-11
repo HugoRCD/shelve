43895945d3fa8f7f49710af5921fe6864baa806d
HugoRCD <hugo.richard@epitech.eu>
2024-10-11 16:59:12 +0200

    fix: It's possible to create infinite empty team

--- apps/app/server/api/teams/index.post.ts
+++ apps/app/server/api/teams/index.post.ts
@@ -5,6 +5,7 @@ import { createTeam } from '~~/server/app/teamsService'
 export default eventHandler(async (event: H3Event) => {
   const { user } = event.context
   const createTeamInput = await readBody(event) as CreateTeamInput
+  if (!createTeamInput.name) throw createError({ statusCode: 400, statusMessage: 'Cannot create team without name' })
   createTeamInput.name = createTeamInput.name.trim()
   return await createTeam(createTeamInput, user.id)
 })
