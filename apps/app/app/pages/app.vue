2121d192929b891375438d03ed20bf03b7704dc4
HugoRCD <hugo.richard@epitech.eu>
2024-10-14 11:47:26 +0200

    feat: Detect if user is online or not

--- apps/app/app/pages/app.vue
+++ apps/app/app/pages/app.vue
@@ -3,15 +3,18 @@ definePageMeta({
   layout: 'app',
   middleware: 'protected',
 })
+const { isOnline } = useNetwork()
 </script>
 
 <template>
   <div class="w-full">
-    <Suspense>
-      <NuxtPage />
-      <template #fallback>
-        Loading...
-      </template>
-    </Suspense>
+    <NuxtPage v-if="isOnline" />
+    <div v-else class="flex h-full flex-col items-center justify-center gap-2 p-5">
+      <UIcon name="lucide:cloud-off" class="size-10 text-gray-400" />
+      <span class="text-sm italic">No internet connection</span>
+      <p class="hidden text-center text-gray-400 sm:block">
+        Please check your internet connection
+      </p>
+    </div>
   </div>
 </template>
