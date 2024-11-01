06e924caa3143f440812b63f062e54c94b8b36ed
HugoRCD <hugo.richard@epitech.eu>
2024-11-01 16:36:43 +0100

    Resend service refacto

--- apps/shelve/server/services/resend.service.ts
+++ apps/shelve/server/services/resend.service.ts
@@ -2,30 +2,50 @@ import { Resend } from 'resend'
 import { render } from '@vue-email/render'
 import verifyOtp from '~~/server/emails/verifyOtp.vue'
 
-export async function sendOtp(email: string, otp: string) {
-  const runtimeConfig = useRuntimeConfig()
-  const resend = new Resend(runtimeConfig.private.resendApiKey)
-  const { siteUrl } = runtimeConfig.public
-  let template
-  try {
-    template = await render(verifyOtp, {
-      otp,
-      redirectUrl: `${siteUrl}/login?email=${email}&otp=${otp}`,
-    })
-  } catch (error) {
-    template = `<h1>OTP: ${otp}</h1>`
+export class EmailService {
+
+  private readonly resend: Resend
+  private readonly siteUrl: string
+  private readonly SENDER = 'HugoRCD <contact@hrcd.fr>'
+
+  constructor() {
+    const config = useRuntimeConfig()
+    this.resend = new Resend(config.private.resendApiKey)
+    this.siteUrl = config.public.siteUrl
   }
 
-  try {
-    await resend.emails.send({
-      from: 'HugoRCD <contact@hrcd.fr>',
-      to: [email],
-      subject: 'Welcome to Shelve!',
-      html: template,
-    }).then((response) => {
-      console.log('Email sent: ', response)
-    })
-  } catch (error) {
-    console.log('Error sending email: ', error)
+  /**
+   * Send OTP verification email
+   */
+  async sendOtp(email: string, otp: string): Promise<void> {
+    const template = await this.generateTemplate(email, otp)
+
+    try {
+      await this.resend.emails.send({
+        from: this.SENDER,
+        to: [email],
+        subject: 'Welcome to Shelve!',
+        html: template,
+      }).then((response) => {
+        console.log('Email sent: ', response)
+      })
+    } catch (error) {
+      console.log('Error sending email: ', error)
+    }
   }
+
+  /**
+   * Generate email template
+   */
+  private async generateTemplate(email: string, otp: string): Promise<string> {
+    try {
+      return await render(verifyOtp, {
+        otp,
+        redirectUrl: `${this.siteUrl}/login?email=${email}&otp=${otp}`,
+      })
+    } catch (error) {
+      return `<h1>OTP: ${otp}</h1>`
+    }
+  }
+
 }
