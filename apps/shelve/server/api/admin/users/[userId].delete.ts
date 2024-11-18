e4e813d87665c918c16aafa47835e0640d15aa4c
HugoRCD <hugo.richard@epitech.eu>
2024-11-18 22:55:59 +0100

    fix admin feature

--- apps/shelve/server/api/admin/users/[userId].delete.ts
+++ apps/shelve/server/api/admin/users/[userId].delete.ts
@@ -2,12 +2,12 @@ import { zh, z } from 'h3-zod'
 import { UserService } from '~~/server/services/user.service'
 
 export default eventHandler(async (event) => {
-  const { user } = event.context
   const { userId } = await zh.useValidatedParams(event, {
     userId: z.string({
       required_error: 'userId is required',
     }).transform((value) => parseInt(value, 10)),
   })
+  const { user } = event.context
   if (user.id === userId) throw createError({ statusCode: 400, statusMessage: 'you can\'t delete your own account' })
   await new UserService().deleteUserById(userId)
   return {
