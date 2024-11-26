e15012ac603479ad096bc30c0db63c45ed80a7bb
HugoRCD <hugo.richard@epitech.eu>
2024-11-26 19:43:48 +0100

    fix type

--- apps/shelve/server/utils/drizzle.ts
+++ apps/shelve/server/utils/drizzle.ts
@@ -5,6 +5,7 @@ import * as schema from '../database/schema'
 export { sql, eq, and, or, ilike, like, not, inArray, asc, desc } from 'drizzle-orm'
 
 export function useDrizzle() {
+  // @ts-expect-error missing type
   const hyperdrive = process.env.POSTGRES as Hyperdrive | undefined
   const dbUrl = process.env.NUXT_POSTGRES_URL
   if (!dbUrl) {
