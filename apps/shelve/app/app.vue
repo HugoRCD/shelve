10e01b95341723e2dc0c56d782098ab9c0753d71
HugoRCD <hugo.richard@epitech.eu>
2024-12-14 00:02:43 +0100

    better easing

--- apps/shelve/app/app.vue
+++ apps/shelve/app/app.vue
@@ -41,12 +41,12 @@ defineShortcuts({
             <NuxtPage />
           </NuxtLayout>
           <template #fallback>
-            <div class="size-full flex items-center justify-center">
+            <div class="size-full flex flex-col gap-4 items-center justify-center">
               <div class="relative">
-                <div class="absolute -inset-3 rounded-full border border-neutral-200/60 dark:border-neutral-700/60 animate-ripple-1" />
-                <div class="absolute -inset-1 rounded-full border border-neutral-200/70 dark:border-neutral-700/70 animate-ripple-2" />
-                <div class="absolute -inset-0.5 rounded-full border border-neutral-200/80 dark:border-neutral-700/80 animate-ripple-3" />
-                <div class="relative rounded-full border-1 border-neutral-200/90 dark:border-neutral-800 bg-white dark:bg-neutral-900 p-4 shadow-sm dark:shadow-lg">
+                <div class="absolute -z-1 -inset-5 rounded-full border border-neutral-200/60 dark:border-neutral-700/60 animate-ripple-1 opacity-0" />
+                <div class="absolute -z-1 -inset-3 rounded-full border border-neutral-200/70 dark:border-neutral-700/70 animate-ripple-2 opacity-0" />
+                <div class="absolute -z-1 -inset-1 rounded-full border border-neutral-200/80 dark:border-neutral-700/80 animate-ripple-3 opacity-0" />
+                <div class="rounded-full z-10 border-1 border-neutral-200/90 dark:border-neutral-800 bg-white dark:bg-neutral-900 p-4 shadow-sm dark:shadow-lg">
                   <Icon name="custom:shelve" class="size-8 animate-pulse" />
                 </div>
               </div>
