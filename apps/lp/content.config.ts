42f6d73604fb9bdbf9bbf66a443ce20b4701626e
HugoRCD <hugo.richard@epitech.eu>
2025-01-21 20:13:25 +0100

    use nuxt/seo lts

--- apps/lp/content.config.ts
+++ apps/lp/content.config.ts
@@ -1,23 +1,26 @@
 import { defineCollection, z } from '@nuxt/content'
+import { asSeoCollection } from '@nuxtjs/seo/content'
 
 export const collections = {
-  content: defineCollection({
-    type: 'page',
-    source: '**/*',
-    schema: z.object({
-      navigation: z.object({
-        title: z.string().optional(),
-      }),
-      links: z.array(z.object({
-        label: z.string(),
-        icon: z.string(),
-        avatar: z.object({
-          src: z.string(),
-          alt: z.string()
-        }).optional(),
-        to: z.string(),
-        target: z.string().optional()
-      }))
+  content: defineCollection(
+    asSeoCollection({
+      type: 'page',
+      source: '**/*',
+      schema: z.object({
+        navigation: z.object({
+          title: z.string().optional(),
+        }),
+        links: z.array(z.object({
+          label: z.string(),
+          icon: z.string(),
+          avatar: z.object({
+            src: z.string(),
+            alt: z.string()
+          }).optional(),
+          to: z.string(),
+          target: z.string().optional()
+        }))
+      })
     })
-  }),
+  )
 }
