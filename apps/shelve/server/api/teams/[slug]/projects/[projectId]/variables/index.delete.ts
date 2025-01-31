2cf844dd967c80497d2ecd6a1a419f2b5e74c48f
HugoRCD <hugo.richard@epitech.eu>
2025-01-31 23:24:41 +0100

    optimisation

--- apps/shelve/server/api/teams/[slug]/projects/[projectId]/variables/index.delete.ts
+++ apps/shelve/server/api/teams/[slug]/projects/[projectId]/variables/index.delete.ts
@@ -5,7 +5,8 @@ export default defineEventHandler(async (event) => {
   const { variables } = await readValidatedBody(event, z.object({
     variables: z.array(z.number()).min(1).max(100),
   }).parse)
-  await Promise.all(variables.map(id => new VariablesService(event).deleteVariable(id)))
+  const variablesService = new VariablesService(event)
+  await Promise.all(variables.map(id => variablesService.deleteVariable(id)))
   return {
     statusCode: 200,
     message: 'Variables deleted',
