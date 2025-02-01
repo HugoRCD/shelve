3bd19052973b36607b8c6a496ec3b5aaa41f35a2
HugoRCD <hugo.richard@epitech.eu>
2025-02-01 11:09:16 +0100

    up

--- packages/cli/src/services/base.ts
+++ packages/cli/src/services/base.ts
@@ -64,7 +64,7 @@ export abstract class BaseService {
       this.api = ofetch.create({
         baseURL,
         headers: {
-          Cookie: `authToken=she_1_BNmlQ67yR7tg5EDCy5oTSWtSt`
+          Cookie: `authToken=${config.token}`
         },
         onResponseError: ErrorService.handleApiError
       })
