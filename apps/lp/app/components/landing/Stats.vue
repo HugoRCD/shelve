67978f950d1cef5323cb391a6848dd7aefb1ed2f
HugoRCD <hugo.richard@epitech.eu>
2025-02-04 10:12:45 +0100

    refactor nuxt-visitors

--- apps/lp/app/components/landing/Stats.vue
+++ apps/lp/app/components/landing/Stats.vue
@@ -60,24 +60,30 @@ const finalStats = computed(() => [
   <div>
     <ClientOnly>
       <Teleport defer to="#visitors">
-        <div class="fixed z-[999] text-neutral-500 text-xs flex gap-2 items-center justify-center bottom-2 left-0 w-full">
-          <span class="relative flex size-2">
-            <span
-              class="absolute bg-green-50 inline-flex size-full animate-ping rounded-full opacity-75"
+        <div class="fixed z-[999] items-center justify-center max-sm:top-2 sm:bottom-2 left-2 w-full">
+          <NuxtLink
+            to="https://github.com/HugoRCD/nuxt-visitors"
+            target="_blank"
+            class="text-neutral-500 text-xs w-fit flex items-center gap-2 cursor-pointer hover:text-neutral-200 transition-colors duration-300"
+          >
+            <span class="relative flex size-2">
+              <span
+                class="absolute bg-green-50 inline-flex size-full animate-ping rounded-full opacity-75"
+              />
+              <span
+                class="relative bg-green-500 inline-flex size-2 scale-90 rounded-full"
+              />
+            </span>
+            <span class="sm:flex hidden">
+              Active visitors:
+            </span>
+            <NumberFlow
+              class="text-sm font-bold font-mono"
+              :value="visitors ?? 0"
+              continuous
+              will-change
             />
-            <span
-              class="relative bg-green-500 inline-flex size-2 scale-90 rounded-full"
-            />
-          </span>
-          <span>
-            Active visitors:
-          </span>
-          <NumberFlow
-            class="text-sm font-bold font-mono"
-            :value="visitors ?? 0"
-            continuous
-            will-change
-          />
+          </NuxtLink>
         </div>
       </Teleport>
     </ClientOnly>
