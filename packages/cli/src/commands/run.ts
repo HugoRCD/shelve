789e59070fc7db3d8866f57db61cea69616e4330
HugoRCD <hugo.richard@epitech.eu>
2025-02-21 13:40:55 +0100

    up

--- packages/cli/src/commands/run.ts
+++ packages/cli/src/commands/run.ts
@@ -1,14 +1,15 @@
-import { spawn } from 'node:child_process'
+import { x } from 'tinyexec'
 import { defineCommand } from 'citty'
-import { intro, outro } from '@clack/prompts'
+import { intro } from '@clack/prompts'
 import type { Environment, EnvVar } from '@types'
+import consola from 'consola'
 import { loadShelveConfig } from '../utils'
 import { EnvironmentService, EnvService, ProjectService } from '../services'
 
 export default defineCommand({
   meta: {
     name: 'run',
-    description: 'Inject secrets from Shelve into your application process',
+    description: '[experimental] Inject secrets from Shelve into your application process',
   },
   args: {
     command: {
@@ -44,43 +45,23 @@ export default defineCommand({
     }
 
     try {
-      outro(`Running command with ${environment.name} environment`)
-
-      const childProcess = spawn(`nr ${args.command}`, {
-        env: processEnv,
-        stdio: 'inherit',
-        shell: true,
-        detached: false
-      })
-
-      process.on('SIGINT', () => {
-        process.kill(-childProcess.pid!, 'SIGINT')
-      })
-
-      process.on('SIGTERM', () => {
-        process.kill(-childProcess.pid!, 'SIGTERM')
-      })
-
-      childProcess.on('error', (error: any) => {
-        console.error('Failed to start process:', error)
-        cleanup(childProcess)
-        process.exit(1)
-      })
-
-      childProcess.on('exit', (code: number | null, signal: string | null) => {
-        if (signal) {
-          cleanup(childProcess)
-          process.exit(0)
-        } else if (code !== 0) {
-          console.error(`Process exited with code ${code}`)
-          cleanup(childProcess)
-          process.exit(code ?? 1)
+      const proc = x('nr', [args.command], {
+        nodeOptions: {
+          env: processEnv,
+          stdio: 'inherit'
         }
-        cleanup(childProcess)
+      })
+
+      const abortController = new AbortController()
+      process.on('SIGINT', () => {
+        consola.info('SIGINT received, aborting process')
+        abortController.abort()
+        proc.kill()
         process.exit(0)
       })
+
+      await proc
     } catch (error) {
-      console.error('Error running command:', error)
       process.exit(1)
     }
   }
@@ -91,12 +72,4 @@ function formatEnvVars(variables: EnvVar[]): NodeJS.ProcessEnv {
     ...acc,
     [key]: value
   }), {})
-}
-
-function cleanup(childProcess: any): void {
-  try {
-    if (childProcess && !childProcess.killed) {
-      process.kill(-childProcess.pid!, 'SIGKILL')
-    }
-  } catch (error) { /* empty */ }
 }
