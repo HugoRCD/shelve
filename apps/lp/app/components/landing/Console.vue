ffd0c5e8909e9520e38b3e84e2187498aae0b56d
HugoRCD <hugo.richard@epitech.eu>
2025-04-11 23:46:13 +0200

    up

--- apps/lp/app/components/landing/Console.vue
+++ apps/lp/app/components/landing/Console.vue
@@ -26,8 +26,8 @@ const selected = ref(commands.value[0])
 <template>
   <BgHighlight class="[mask-image:linear-gradient(to_bottom,white_50%,transparent)]">
     <div class="bg-(--ui-bg)/50 p-2 w-[400px] rounded-md">
-      <div class="screen-container rounded-md bg-(--ui-bg) flex size-full flex-col">
-        <div v-for="command in commands" :key="command.id" class="command-item" :class="{ selected: selected?.id === command.id }" @click="selected = command">
+      <div class="screen-container p-2 rounded-md bg-(--ui-bg) flex size-full flex-col gap-2">
+        <div v-for="command in commands" :key="command.id" class="cursor-pointer flex items-center gap-3 rounded-md px-2 py-1 hover:bg-(--ui-bg-muted)" :class="{ 'bg-(--ui-bg-accented)/40': selected?.id === command.id }" @click="selected = command">
           <div class="flex items-center gap-3">
             <UIcon :name="command.icon" class="text-(--ui-text-muted)" />
             <div>
@@ -48,25 +48,3 @@ const selected = ref(commands.value[0])
     </div>
   </BgHighlight>
 </template>
-
-<style scoped>
-@reference '../../assets/css/index.css'
-
-.screen-container {
-  @apply bg-(--ui-bg) m-2 rounded-md overflow-hidden;
-  box-shadow: inset 3px 3px 5px rgba(173, 173, 173, 0.3);
-}
-
-.command-item {
-  @apply cursor-pointer flex items-center gap-3 rounded-md m-2 px-2 py-1;
-  @apply hover:bg-(--ui-bg-muted) relative overflow-hidden;
-}
-
-.command-item.selected {
-  @apply bg-(--ui-bg-accented)/40;
-}
-
-.dark .screen-container {
-  box-shadow: inset 3px 3px 10px rgb(0 0 0 / 0.2);
-}
-</style>
