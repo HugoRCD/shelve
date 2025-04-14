d037ff837e03b858ce9ccaae837a66d482fcaedd
HugoRCD <hugo.richard@epitech.eu>
2025-04-14 22:02:27 +0200

    up

--- apps/shelve/server/api/github/repos/index.get.ts
+++ apps/shelve/server/api/github/repos/index.get.ts
@@ -5,9 +5,9 @@ export default defineEventHandler(async (event) => {
   const { q } = await getValidatedQuery(event, z.object({
     q: z.string().optional(),
   }).parse)
-  const repos = await new GithubService(event).getUserRepos(user.id)
+  const repos = await new GithubService(event).getUserRepos(event, user.id)
 
-  return repos.filter((repo) => {
+  return repos?.filter((repo) => {
     const name = repo.name.toLowerCase()
     const query = q?.toLowerCase()
     return !query || name.includes(query)
