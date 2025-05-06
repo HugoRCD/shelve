14ed9d0f9fd39c182df360bb66fd3436d76d42bd
HugoRCD <hugo.richard@epitech.eu>
2025-05-06 21:12:16 +0200

    up

--- apps/base/components/Separator.vue
+++ apps/base/components/Separator.vue
@@ -6,11 +6,11 @@ const { orientation = 'horizontal' } = defineProps<{
 
 <template>
   <div v-if="orientation === 'horizontal'">
-    <USeparator :ui="{ border: 'border-[1px] border-default' }" />
-    <USeparator :ui="{ border: 'border-[1px] border-elevated' }" />
+    <USeparator :ui="{ border: 'border-[1px] border-(--ui-bg)' }" />
+    <USeparator :ui="{ border: 'border-[1px] border-(--ui-bg-elevated)' }" />
   </div>
   <div v-else class="flex h-full">
-    <USeparator orientation="vertical" class="h-auto" :ui="{ border: 'border-[4px] border-default' }" />
-    <USeparator orientation="vertical" class="h-auto" :ui="{ border: 'border-[4px] border-elevated' }" />
+    <USeparator orientation="vertical" class="h-auto" :ui="{ border: 'border-[4px] border-(--ui-bg)' }" />
+    <USeparator orientation="vertical" class="h-auto" :ui="{ border: 'border-[4px] border-(--ui-bg-elevated)' }" />
   </div>
 </template>
