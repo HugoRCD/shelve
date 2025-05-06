d94e8d5fe29598053eb80a0637d69defce52c5d0
HugoRCD <hugo.richard@epitech.eu>
2025-05-06 23:17:42 +0200

    up

--- apps/shelve/server/utils/drizzle.ts
+++ apps/shelve/server/utils/drizzle.ts
@@ -1,16 +1,17 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
 
 import * as schema from '../database/schema'
 
 export { sql, eq, and, or, ilike, like, not, inArray, asc, desc } from 'drizzle-orm'
 
 export function useDrizzle() {
-  return drizzle({
-    connection: {
-      connectionString: process.env.DATABASE_URL
-    },
-    schema
-  })
+  // @ts-expect-error missing type
+  const hyperdrive = process.env.POSTGRES as Hyperdrive | undefined
+  const dbUrl = process.env.NUXT_POSTGRES_URL
+  if (!dbUrl) {
+    throw createError('Missing `POSTGRES` hyperdrive binding or `NUXT_POSTGRES_URL` env variable')
+  }
+  return drizzle(dbUrl, { schema })
 }
 
 export const tables = schema
