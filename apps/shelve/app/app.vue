9c01abe814e1f3deb8431a77c0758b02e25857a6
HugoRCD <hugo.richard@epitech.eu>
2025-05-28 21:23:17 +0200

    feat(app): use motion for animations

--- apps/shelve/app/app.vue
+++ apps/shelve/app/app.vue
@@ -20,26 +20,28 @@ if (import.meta.client) setPrefersReducedMotion(reduceMotion.value)
   <Html lang="en">
     <Body class="overscroll-y-none selection:bg-primary relative overflow-x-hidden selection:text-inverted">
       <ShelveMeta title="Application" title-template="%s | Shelve" />
-      <UApp :tooltip="{ delayDuration: 0 }">
-        <Suspense>
-          <NuxtLayout>
-            <NuxtPage />
-          </NuxtLayout>
-          <template #fallback>
-            <div class="size-full flex flex-col gap-4 items-center justify-center">
-              <div class="relative">
-                <div class="absolute -z-1 -inset-5 rounded-full border border-default animate-ripple-1 opacity-0" />
-                <div class="absolute -z-1 -inset-3 rounded-full border border-default animate-ripple-2 opacity-0" />
-                <div class="absolute -z-1 -inset-1 rounded-full border border-default animate-ripple-3 opacity-0" />
-                <div class="rounded-full flex items-center justify-center z-10 border-1 border-default bg-muted p-4 shadow-sm dark:shadow-lg">
-                  <Icon name="custom:shelve" class="size-8 animate-pulse" />
+      <MotionConfig :reduce-motion="reduceMotion ? 'always' : 'never'">
+        <UApp :tooltip="{ delayDuration: 0 }">
+          <Suspense>
+            <NuxtLayout>
+              <NuxtPage />
+            </NuxtLayout>
+            <template #fallback>
+              <div class="size-full flex flex-col gap-4 items-center justify-center">
+                <div class="relative">
+                  <div class="absolute -z-1 -inset-5 rounded-full border border-default animate-ripple-1 opacity-0" />
+                  <div class="absolute -z-1 -inset-3 rounded-full border border-default animate-ripple-2 opacity-0" />
+                  <div class="absolute -z-1 -inset-1 rounded-full border border-default animate-ripple-3 opacity-0" />
+                  <div class="rounded-full flex items-center justify-center z-10 border-1 border-default bg-muted p-4 shadow-sm dark:shadow-lg">
+                    <Icon name="custom:shelve" class="size-8 animate-pulse" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </template>
-        </Suspense>
-        <Toaster close-button position="top-center" />
-      </UApp>
+            </template>
+          </Suspense>
+          <Toaster close-button position="top-center" />
+        </UApp>
+      </MotionConfig>
     </Body>
   </Html>
 </template>
