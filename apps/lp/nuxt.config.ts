c93e5e70bb9860c1dde5315e45c5c907fc602fc1
HugoRCD <hugo.richard@epitech.eu>
2025-06-19 14:06:02 +0200

    up

--- apps/lp/nuxt.config.ts
+++ apps/lp/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     '/docs': { redirect: { statusCode: 301, to: '/docs/getting-started' } },
     '/docs/core-features': { redirect: { statusCode: 301, to: '/docs/core-features/environments' } },
     '/docs/integrations': { redirect: { statusCode: 301, to: '/docs/integrations/github' } },
-    '/docs/self-hosting': { redirect: { statusCode: 301, to: '/docs/self-hosting/docker' } },
+    '/docs/self-hosting': { redirect: { statusCode: 301, to: '/docs/self-hosting/vercel' } },
   },
 
   future: {
