ca4e164a1e67d7f139b4656145d5107ccbe3b4df
Hugo Richard <hugo.richard@vercel.com>
2026-01-24 11:30:55 +0000

    fix CLI auth

--- apps/shelve/server/plugins/session.ts
+++ apps/shelve/server/plugins/session.ts
@@ -1,4 +1,15 @@
 export default defineNitroPlugin(() => {
+  sessionHooks.hook('fetch', async (session, event) => {
+    if (session.user) return
+
+    const authToken = getCookie(event, 'authToken')
+    if (!authToken) return
+
+    const user = await getUserByAuthToken(authToken, event)
+    session.user = user
+    session.loggedInAt = new Date()
+  })
+
   sessionHooks.hook('clear', (session, event) => {
     deleteCookie(event, 'defaultTeamSlug')
   })
