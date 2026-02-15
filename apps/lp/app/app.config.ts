cbed8290b1ad976d6ba96fa60a85f52a7e5827df
Hugo <hugo.richard@vercel.com>
2026-02-15 19:03:54 +0000

    feat(docs): use docus (#705)

--- apps/lp/app/app.config.ts
+++ apps/lp/app/app.config.ts
@@ -19,5 +19,33 @@ export default defineAppConfig({
         container: 'py-10 sm:py-10 lg:py-16',
       }
     }
-  }
+  },
+  socials: {
+    github: 'https://github.com/HugoRCD',
+    twitter: 'https://x.com/hugorcd',
+  },
+  toc: {
+    bottom: {
+      title: 'Resources',
+      links: [
+        {
+          icon: 'i-lucide-file-text',
+          label: 'llms.txt',
+          to: '/llms.txt',
+          target: '_blank',
+        },
+        {
+          icon: 'i-lucide-heart-handshake',
+          label: 'Contributing',
+          to: '/docs/contributing',
+        },
+        {
+          icon: 'i-lucide-cpu',
+          label: 'MCP Server',
+          to: '/mcp/deeplink',
+          external: true,
+        }
+      ],
+    },
+  },
 })
